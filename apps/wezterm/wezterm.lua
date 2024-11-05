49f9c6eb475b43730060e87267552d2055a91d8f
Jan Bücker <j.buecker@shopware.com>
2024-11-05 09:09:36 +0100

    Remove monaspace font

--- apps/wezterm/wezterm.lua
+++ apps/wezterm/wezterm.lua
@@ -3,8 +3,6 @@ local config = wezterm.config_builder()
 
 config.default_prog = { wezterm.home_dir .. "/.nix-profile/bin/zsh" }
 config.font_size = 20
-config.font = wezterm.font("Monaspace Neon")
-config.line_height = 1.2
 config.pane_focus_follows_mouse = true
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
