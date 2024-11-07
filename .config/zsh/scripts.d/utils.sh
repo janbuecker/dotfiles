687e5b94661a9327e963472f6cbfd06a0e7ae540
Jan Bücker <j.buecker@shopware.com>
2024-11-07 14:57:48 +0100

    Add cdenv script

--- .config/zsh/scripts.d/utils.sh
+++ .config/zsh/scripts.d/utils.sh
@@ -11,3 +11,11 @@ mfa() {
 gopro() {
     export GORELEASER_KEY=$(op item get Goreleaser --fields "label=license key")
 }
+
+cdenv() {
+    if [[ $(pwd) =~ /staging/ ]]; then
+        cd $(pwd | sed 's!/staging/!/production/!')
+    else
+        cd $(pwd | sed 's!/production/!/staging/!')
+    fi
+}
